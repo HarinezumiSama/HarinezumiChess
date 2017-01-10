1b3ec22f063c26599a5c498e7640472641095652
vmcl <none@none>
2017-01-09 21:48:16 -0500

    Created skeleton of the main project

---
+++ src/.common/CommonAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("HarinezumiChess")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Vitalii Maklai")]
+[assembly: AssemblyProduct("HarinezumiChess")]
+[assembly: AssemblyCopyright("Copyright © Vitalii Maklai 2014-2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
---
+++ src/HarinezumiChess/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HarinezumiChess")]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("4558239c-acff-4961-b166-597c714a325a")]
