6b726f05091bec77bca9cc1546429215fb814375
vmcl <none@none>
2017-01-11 02:18:18 -0500

    Added an empty unit test project

---
+++ src/HarinezumiChess.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HarinezumiChess.Tests")]
+[assembly: CLSCompliant(false)]
+[assembly: Guid("675d501f-49df-4be7-bb9a-eb7b02e11804")]
